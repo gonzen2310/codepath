e7b6f5d7d6ddda86bc8d81e7fadcf967c1a61f75
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-10-18 17:28:03 -0400

    first commit

---
+++ flixster/app/src/main/java/com/gonchi/flixster/MainActivity.java
@@ -0,0 +1,71 @@
+package com.gonchi.flixster;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.gonchi.flixster.adapters.MovieAdapter;
+import com.gonchi.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+import static com.gonchi.flixster.models.Movie.fromJsonArray;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String MOVIE_API_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
+    List<Movie> movies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        movies = new ArrayList<>();
+
+        RecyclerView rvMovies = findViewById(R.id.rvMovies);
+        final MovieAdapter movieAdapter = new MovieAdapter(this, movies);
+        rvMovies.setAdapter(movieAdapter);
+        rvMovies.setLayoutManager(new LinearLayoutManager(this));
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(MOVIE_API_URL, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.d(TAG, "onSuccess");
+                        JSONObject jsonObject =  json.jsonObject;
+                        try {
+                            JSONArray results = jsonObject.getJSONArray("results");
+                            Log.i(TAG, "Results " + results.toString());
+                            movies.addAll(Movie.fromJsonArray(results));
+                            movieAdapter.notifyDataSetChanged();
+                            Log.i(TAG, "Array " + movies);
+
+                        } catch (JSONException e) {
+                            Log.e(TAG, "hit json exception");
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Log.d(TAG, "onFailure");
+                    }
+                }
+        );
+    }
+}
---
+++ flixster/app/src/main/java/com/gonchi/flixster/models/Movie.java
@@ -0,0 +1,49 @@
+package com.gonchi.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    private String backdropPath;
+    private String posterPath;
+    private String title;
+    private String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        this.backdropPath = jsonObject.getString("backdrop_path");
+        this.posterPath = jsonObject.getString("poster_path");
+        this.title = jsonObject.getString("title");
+        this.overview = jsonObject.getString("overview");
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath ) ;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath  ) ;
+    }
+
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+}
